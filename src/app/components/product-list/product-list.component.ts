4431fd9343be567dfd9971dea1291088c06bdda6
Valentin Piho <val.piho@gmail.com>
2020-11-24 15:23:10 +0200

    Moving menu to own component

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -12,6 +12,7 @@ export class ProductListComponent implements OnInit {
 
   products: Product[];
   currentCategoryId: number;
+  currentCategoryName: string;
   constructor(private productService: ProductService,
               private route: ActivatedRoute) { }
 
@@ -21,12 +22,14 @@ export class ProductListComponent implements OnInit {
     });
   }
 
-  listProducts() {
+  private listProducts() {
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
     if (hasCategoryId) {
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
+      this.currentCategoryName = this.route.snapshot.paramMap.get('name');
     } else {
       this.currentCategoryId = 1;
+      this.currentCategoryName = 'Books';
     }
     this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
